55db5ecbc4b2e7cef9860ce75f0bfd60abc554f9
toni.dermawan <toni_d_y@yahoo.co.id>
2013-01-12 21:19:58 +0700

    edit index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-  print 'tes';
-  phpinfo();
+  echo 'Ini beneran pake PHP lho (in bahasa) <br/>';
+  echo 'Current PHP version: ' . phpversion();
+  
+  echo '<br/>Created by @tonidermawan';
+  //phpinfo();
 ?>
